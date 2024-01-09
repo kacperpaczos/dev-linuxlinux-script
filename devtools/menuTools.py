847db154351bdfa304b01c4f3a679470abf94faf
Kacper Paczos <kacper-paczos@linux.pl>
2024-01-10 00:13:45 +0100

    Added thefuck

--- devtools/menuTools.py
+++ devtools/menuTools.py
@@ -11,6 +11,7 @@ class toolsMenu:
             "3": self.git_build_install,
             "4": self.enable_daily_builds,
             "5": self.install_biblioteca,
+            "6": self.install_the_fuck,
             "*": self.exit
         }
 
@@ -29,10 +30,15 @@ class toolsMenu:
     def install_biblioteca(self):
         os.system('flatpak install flathub app.drey.Biblioteca')
 
+    def install_the_fuck(self):
+        os.system('sudo apt update')
+        os.system('sudo apt install python3-dev python3-pip python3-setuptools')
+        os.system('pip3 install thefuck --user')
+
     def run(self):
         self.console.clear()
         while True:
-            self.console.print("[1] Install lib*dev\n[2] Install dev-tools\n[3] Git, build, install\n[4] Enable daily builds\n[5] Install Biblioteca\n[*] Any key to exit")
+            self.console.print("[1] Install lib*dev\n[2] Install dev-tools\n[3] Git, build, install\n[4] Enable daily builds\n[5] Install Biblioteca\n[6] Install 'thefuck'\n[*] Any key to exit")
             choice = Prompt.ask("Choose an option", choices=list(self.options.keys()))
             if choice == "*":
                 break
